5c6a796448419fd82b212babacc1490e84316ba3
lais-sue <laissunaga@gmail.com>
2022-04-01 10:57:59 -0400

    Fixed hours

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 0) {
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if (minutes < 0) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
   let days = [
