90662b4d35d37de83c5b9c464f3ed1d505ac8b73
lais-sue <laissunaga@gmail.com>
2022-04-04 14:25:52 -0400

    Displayed forecast data using API

--- src/app.js
+++ src/app.js
@@ -22,34 +22,55 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHMTL = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed"];
-  days.forEach(function (day) {
-    forecastHMTL =
-      forecastHMTL +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHMTL =
+        forecastHMTL +
+        `
 <div class="col-2"> 
   <div class="weather-forecast-date">
-     ${day}
+     ${formatDay(forecastDay.dt)}
   </div>
-   <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="42">
+   <img src="https://openweathermap.org/img/wn/${
+     forecastDay.weather[0].icon
+   }@2x.png" alt="" width="42">
   <div class="weather-forecast-temperatures">
     <span class="weather-forecast-temperature-max">
-       18°
+       ${Math.round(forecastDay.temp.max)}°
     </span>
      <span class="weather-forecast-temperature-min">
-       12°
+       ${Math.round(forecastDay.temp.min)}°
     </span>
   </div>
 </div>
 `;
+    }
   });
 
   forecastHMTL = forecastHMTL + `</div>`;
   forecastElement.innerHTML = forecastHMTL;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "bc9c173a3ba1b56ece5993ed8c0643da";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -74,6 +95,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -116,4 +139,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 search("Toronto");
-displayForecast();
