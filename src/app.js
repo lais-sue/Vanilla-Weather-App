9773e9a0b0307abe2cf3c29a8893610c07609b85
lais-sue <laissunaga@gmail.com>
2022-04-04 11:50:13 -0400

    Removed forecast from HMTL and added in JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHMTL = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed"];
+  days.forEach(function (day) {
+    forecastHMTL =
+      forecastHMTL +
+      `
+<div class="col-2"> 
+  <div class="weather-forecast-date">
+     ${day}
+  </div>
+   <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="42">
+  <div class="weather-forecast-temperatures">
+    <span class="weather-forecast-temperature-max">
+       18°
+    </span>
+     <span class="weather-forecast-temperature-min">
+       12°
+    </span>
+  </div>
+</div>
+`;
+  });
+
+  forecastHMTL = forecastHMTL + `</div>`;
+  forecastElement.innerHTML = forecastHMTL;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -86,3 +116,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 search("Toronto");
+displayForecast();
